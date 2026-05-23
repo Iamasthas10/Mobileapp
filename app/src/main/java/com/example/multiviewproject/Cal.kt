e94eba2fddc22683c3e8280428632d5b42ed163f
Astha <iamasthas10@gmail.com>
2026-05-23 23:43:42 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/multiviewproject/Cal.kt
@@ -0,0 +1,12 @@
+package com.example.multiviewproject
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Cal : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+    }
+}
